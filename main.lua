d5e291c39ae039c80ab7459e7f32a6bc99976990
Adam Bieszk <adambieszk6@gmail.com>
2026-01-14 11:51:05 +0100

    fix(path): change paths to boss icons

--- main.lua
+++ main.lua
@@ -1,4 +1,4 @@
-local modName = "Fall From Grace: Enhanced Boss Bars addon"
+local modName = "Fall From Grace boss icons"
 local FFGBossbar = RegisterMod(modName, 1)
 
 -- #region check of required mods being installed
@@ -23,46 +23,46 @@ if HPBars then
     --#region Boiler
         --#region Charlie
     HPBars.BossDefinitions["360.3"] = { 
-        sprite = "gfx/bosses/boiler/charlie/charlieicon.png",
+        sprite = "gfx/ui/bosshp_icons/boiler/charlie/charlie.png",
         offset = Vector(-10, 0)
     }
         --#endregion
         --#region Blister Twins (Affusion and Salmon)
     HPBars.BossDefinitions["360.10"] = {
-        sprite = "gfx/bosses/boiler/blister twins/affusionicon.png",
+        sprite = "gfx/ui/bosshp_icons/boiler/blister twins/affusion.png",
         offset = Vector(-7,0)
     }
 
     HPBars.BossDefinitions["360.11"] = {
-        sprite = "gfx/bosses/boiler/blister twins/salmonicon.png",
+        sprite = "gfx/ui/bosshp_icons/boiler/blister twins/salmon.png",
         offset = Vector(-6,0)
     }
         --#endregion
         --#region Creem & Mate
     HPBars.BossDefinitions["360.20"] = {
-        sprite = "gfx/bosses/boiler/creem/creemicon.png",
+        sprite = "gfx/ui/bosshp_icons/boiler/creem/creem.png",
         offset = Vector(-39,0)
     }
     HPBars.BossDefinitions["360.21"] = {
-        sprite = "gfx/bosses/boiler/creem/mateicon.png",
+        sprite = "gfx/ui/bosshp_icons/boiler/creem/mate.png",
         offset = Vector(-7,0)
     }
         --#endregion
         --#region Pipeline
     HPBars.BossDefinitions["360.30"] = {
-        sprite = "gfx/bosses/boiler/pipeline/tootieicon.png",
+        sprite = "gfx/ui/bosshp_icons/boiler/pipeline/tootie.png",
         offset = Vector(-6,0)
     }
     HPBars.BossDefinitions["360.31"] = {
-        sprite = "gfx/bosses/boiler/pipeline/chubbleicon.png",
+        sprite = "gfx/ui/bosshp_icons/boiler/pipeline/chubble.png",
         offset = Vector(-15,0)
     }
     HPBars.BossDefinitions["360.32"] = {
-        sprite = "gfx/bosses/boiler/pipeline/grufficon.png",
+        sprite = "gfx/ui/bosshp_icons/boiler/pipeline/gruff.png",
         offset = Vector(-15,0)
     }
     HPBars.BossDefinitions["360.33"] = {
-        sprite = "gfx/bosses/boiler/pipeline/smokeeicon.png",
+        sprite = "gfx/ui/bosshp_icons/boiler/pipeline/smokee.png",
         offset = Vector(-15,0)
     }
         --#endregion
@@ -70,37 +70,38 @@ if HPBars then
     --#region Grotto
         --#region Stub
     HPBars.BossDefinitions["360.40"] = {
-        sprite = "gfx/bosses/grotto/stub/stubicon.png",
+        sprite = "gfx/ui/bosshp_icons/grotto/stub/stub.png",
         offset = Vector(0,0)
     }
         --#endregion
         --#region Sloppy Joe
     HPBars.BossDefinitions["360.50"] = {
-        sprite = "gfx/bosses/grotto/sloppy joe/sloppyjoeicon.png",
+        sprite = "gfx/ui/bosshp_icons/grotto/sloppy joe/sloppyjoe.png",
         offset = Vector(0,0)
     }
         --#endregion
         --#region Plumpod II
     HPBars.BossDefinitions["360.60"] = {
-        sprite = "gfx/bosses/grotto/plumpod/plumpodicon.png",
+        sprite = "gfx/ui/bosshp_icons/grotto/plumpod/plumpod.png",
         offset = Vector(0,0)
     }
         --#endregion
         --#region Ms. Guano
     HPBars.BossDefinitions["360.70"] = {
-        sprite = "gfx/bosses/grotto/ms. guano/msguanoicon.png",
+        sprite = "gfx/ui/bosshp_icons/grotto/ms. guano/msguano.png",
         offset = Vector(0,0)
     }
         --#endregion
         --#region Dripilla
     HPBars.BossDefinitions["360.80"] = {
-        sprite = "gfx/bosses/grotto/dripilla/dripillaicon.png",
+        sprite = "gfx/ui/bosshp_icons/grotto/dripilla/dripilla.png",
         offset = Vector(-14,0)
     }
         --#endregion
         --#region Dripilla (toxic chamption)
     -- HPBars.BossDefinitions["360.80"] = {
-    --     sprite = "gfx/bosses/grotto/dripilla/dripillatoxicicon.png",
+    --     sprite = "gfx/ui/bosshp_icons/grotto/dripilla/dripillatoxic.png",
+    --     color = "{_champion}"
     --     offset = Vector(-18,0)
     -- }
         --#endregion
